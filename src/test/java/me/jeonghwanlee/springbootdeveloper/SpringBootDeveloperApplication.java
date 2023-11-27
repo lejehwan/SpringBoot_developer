355f43c54b0409a05273e89d9c45e3b8f6ada242
lejehwan <rdsa257@naver.com>
2023-11-27 22:07:31 +0900

    Initial commit

---
+++ src/test/java/me/jeonghwanlee/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,13 @@
+package me.jeonghwanlee.springbootdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDeveloperApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
