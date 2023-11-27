03012e14243e01a0bbb7bf04c0b00fb8a86e9020
lejehwan <rdsa257@naver.com>
2023-11-27 22:55:18 +0900

    Feat:: dependency settings

--- src/main/java/me/jeonghwanlee/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/jeonghwanlee/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudyApplication {
+public class SpringBootDeveloperApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudyApplication.class, args);
+		SpringApplication.run(SpringBootDeveloperApplication.class, args);
 	}
 
 }
