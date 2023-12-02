3793fa3fdc21d09f22d327076ee5b89e3cf44144
lejehwan <rdsa257@naver.com>
2023-12-02 22:27:54 +0900

    Feat:: config 파일로 security 설정하기

---
+++ src/main/java/me/jeonghwanlee/springbootdeveloper/config/WebSecurityConfig.java
@@ -0,0 +1,61 @@
+package me.jeonghwanlee.springbootdeveloper.config;
+
+import lombok.RequiredArgsConstructor;
+import me.jeonghwanlee.springbootdeveloper.service.UserDetailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+/**
+ * @author jeonghwanlee
+ * @date 2023-12-02
+ */
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final UserDetailService userService;
+
+    // 스프링 시큐리티 기능 비활성화
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return web -> web.ignoring()
+                .requestMatchers(toH2Console())
+                .requestMatchers("/static/**");
+    }
+
+    // 특정 HTTP 요청에 대한 웹 기반 보안 구성
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                // 인증 인가 설정
+                .authorizeRequests()
+                    .requestMatchers("/login", "/signup", "/user").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                // 폼 기반 로그인 설정
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/articles"))
+                // 로그 아웃 설정
+                .logout(logout -> logout.logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true))
+                // csrf 비활성화
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+    // 인증 관리자 관련 설정
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+}
