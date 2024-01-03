2fd4a36e3d12150554dcec535276e8159ff662fa
lejehwan <rdsa257@naver.com>
2024-01-03 22:29:29 +0900

    Fix:: delete to empty test method

--- src/test/java/me/jeonghwanlee/springbootdeveloper/SpringbootdeveloperApplicationTests.java
+++ src/test/java/me/jeonghwanlee/springbootdeveloper/SpringbootdeveloperApplicationTests.java
@@ -1,13 +1,8 @@
 package me.jeonghwanlee.springbootdeveloper;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootdeveloperApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
